74c0e11715b6e1a65610602b0ebdc1f513a8c49c
fededl1995 <casii_fede@live.com.ar>
2024-07-22 17:19:41 -0300

    12

--- scripts.js
+++ scripts.js
@@ -4,20 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
         "multimedia/imagenes/1.jpeg",
         "multimedia/imagenes/2.jpeg",
         "multimedia/imagenes/3.jpeg",
-        "multimedia/imagenes/4.jpeg", 
-        "multimedia/imagenes/5.jpeg", 
-        "multimedia/imagenes/6.jpeg", 
-        "multimedia/imagenes/7.jpeg", 
-        "multimedia/imagenes/8.jpeg", 
-        "multimedia/imagenes/9.jpeg", 
-        "multimedia/imagenes/10.jpeg", 
-        "multimedia/imagenes/11.jpeg", 
+        "multimedia/imagenes/4.jpeg",
+        "multimedia/imagenes/5.jpeg",
+        "multimedia/imagenes/6.jpeg",
+        "multimedia/imagenes/7.jpeg",
+        "multimedia/imagenes/8.jpeg",
+        "multimedia/imagenes/9.jpeg",
+        "multimedia/imagenes/10.jpeg",
+        "multimedia/imagenes/11.jpeg",
         "multimedia/imagenes/12.jpeg",
         "multimedia/imagenes/13.jpeg",
     ];
 
     let currentIndex = 0;
 
+    // Cargar imágenes en el carrusel
     images.forEach(src => {
         const img = document.createElement('img');
         img.src = src;
@@ -38,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         carousel.style.transform = `translateX(${offset}%)`;
     }
 
+    // Controladores de botones
     document.querySelector('.carousel-button.next').addEventListener('click', () => {
         showImage(currentIndex + 1);
     });
