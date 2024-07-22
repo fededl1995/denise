d819bb3b1646eaa2964486f99518a4ade21e27a5
fededl1995 <casii_fede@live.com.ar>
2024-07-22 16:41:08 -0300

    433

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gallery = document.getElementById('gallery');
     const images = [
-        'Multimedia/imagenes/1.jpeg',
+        'multimedia/imagenes/1.jpeg',
         'ruta-de-tu-imagen2.jpg',
         'ruta-de-tu-imagen3.jpg',
         'ruta-de-tu-imagen4.jpg',
