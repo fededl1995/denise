fc9d5283edf52b914054976ea492258a1533a942
fededl1995 <casii_fede@live.com.ar>
2024-07-23 12:16:25 -0300

    kasnfks

--- scripts.js
+++ scripts.js
@@ -4,22 +4,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const prevButton = document.querySelector('.carousel-button.prev');
     const nextButton = document.querySelector('.carousel-button.next');
 
-    let currentIndex = 0;
     const images = [
         "multimedia/imagenes/1.jpeg",
         "multimedia/imagenes/2.jpeg",
-        "multimedia/imagenes/3.jpeg",
+        "multimedia/imagenes/8.jpeg",
         "multimedia/imagenes/4.jpeg",
         "multimedia/imagenes/5.jpeg",
         "multimedia/imagenes/6.jpeg",
         "multimedia/imagenes/7.jpeg",
-        "multimedia/imagenes/8.jpeg",
+        "multimedia/imagenes/3.jpeg",
         "multimedia/imagenes/9.jpeg",
         "multimedia/imagenes/10.jpeg",
         "multimedia/imagenes/11.jpeg",
         "multimedia/imagenes/12.jpeg",
         "multimedia/imagenes/13.jpeg",
-    ];
+     ];
+
+    // Muestra solo las primeras 5 imágenes
+    const visibleImages = images.slice(0, 13);
+
+    let currentIndex = 0;
 
     function updateCarousel() {
         // Ajusta el desplazamiento del carrusel en función del índice actual
@@ -28,20 +32,22 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function showNextImage() {
-        // Avanza al siguiente índice de la imagen
-        currentIndex = (currentIndex + 1) % images.length;
-        updateCarousel();
+        if (currentIndex < visibleImages.length - 1) {
+            currentIndex++;
+            updateCarousel();
+        }
     }
 
     function showPrevImage() {
-        // Retrocede al índice anterior de la imagen
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateCarousel();
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateCarousel();
+        }
     }
 
     function initCarousel() {
-        // Rellena el carrusel con las imágenes
-        carousel.innerHTML = images.map(src => `<img src="${src}" alt="Image">`).join('');
+        // Rellena el carrusel con las primeras 5 imágenes
+        carousel.innerHTML = visibleImages.map(src => `<img src="${src}" alt="Image">`).join('');
         updateCarousel();
     }
 
